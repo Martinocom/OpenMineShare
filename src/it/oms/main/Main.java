91fc895917861f1d1819eda7999ed255acc46e62
Leonardo Papini <leonardopapini96@gmail.com>
2019-08-31 11:23:53 +0200

    Add logger

---
+++ src/it/oms/main/Main.java
@@ -0,0 +1,15 @@
+package it.oms.main;
+
+import org.tinylog.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger.debug("Hello World!");
+        Logger.info("Hello World!");
+        Logger.warn("Hello World!");
+        Logger.error("Hello World!");
+
+    }
+
+}
